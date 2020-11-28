41be4853ad5256b0ed47217baec877a9e74e2485
Anton Skochko <antonskochko@gmail.com>
2020-11-28 16:07:14 +0200

    changed the way components are created/loaded

--- app/app.py
+++ app/app.py
@@ -1,12 +1,8 @@
-from PyQt5 import QtWidgets, uic
-import sys
- 
-class Application:
+from PyQt5 import QtWidgets
+
+from app.gui.components import main
+
+class Application(QtWidgets.QMainWindow, main.Ui_MainWindow):
     def __init__(self):
-        self.app = QtWidgets.QApplication([])
-        self.main_window = uic.loadUi("./app/gui/main_window.ui")
-    
-    def run(self):
-        self.main_window.show()
-        sys.exit(self.app.exec())
- 
+        super().__init__()
+        self.setupUi(self) 
