54d961b95969e4a1e5f8284695e6c4aff9689f3b
1okey <antonskochko@gmail.com>
2024-03-14 22:32:14 +0200

    add actual unit tests

--- darwin_test.go
+++ darwin_test.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDarwinCPU(t *testing.T) {
-	var darwin = GetDarwinReport()
-	fmt.Printf("%v", darwin.Cpu)
-	assert.NotEmpty(t, darwin.Cpu.Vendor.Name)
+func TestInvalidGpuInput(t *testing.T) {
+	cpu := GetDarwinCPU([]string {
+		"core_count: 4",
+	})
+
+	assert.Equal(t, uint8(0), cpu.Cores)
+}
+
+func TestValidGpuInput(t *testing.T) {
+	cpu := GetDarwinCPU([]string {
+		cpu_prefix + "core_count: 4",
+	})
+
+	assert.Equal(t, uint8(4), cpu.Cores)
+}
+
+
+func TestInvalidCpuInput(t *testing.T) {
+	gpu := GetDarwinGPU([]string {
+		"VRAMM: 8 GB",
+	})
+
+	assert.Equal(t, "", gpu.Memory)
+}
+
+func TestValidCpuInput(t *testing.T) {
+	gpu := GetDarwinGPU([]string {
+		"VRAM: 8 GB",
+	})
+
+	assert.Equal(t, "8 GB", gpu.Memory)
 }
