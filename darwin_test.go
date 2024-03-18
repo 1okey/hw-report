b4e9df34b871bf0de59e0c875c0a06f24ea52258
1okey <antonskochko@gmail.com>
2024-03-19 00:53:05 +0200

    add test for memory input parsing

--- darwin_test.go
+++ darwin_test.go
@@ -38,3 +38,30 @@ func TestValidCpuInput(t *testing.T) {
 
 	assert.Equal(t, "8 GB", gpu.Memory)
 }
+
+func TestValidMemInput(t *testing.T) {
+	mem := GetDarwinMemory([]string {
+	"BANK 0/DIMM0:",
+	"",
+    "Size: 8 GB",
+    "Type: DDR4 SO-DIMM",
+    "Speed: 2400 MHz",
+    "Status: OK",
+    "Manufacturer: 0x802C",
+    "Part Number: 0x3841544631473634485A2D324733453220202020",
+    "Serial Number: 0x25250504",
+	"",
+    "BANK 0/DIMM1:",
+	"",
+    "Size: 8 GB",
+    "Type: DDR4 SO-DIMM",
+    "Speed: 2400 MHz",
+    "Status: OK",
+    "Manufacturer: 0x802C",
+    "Part Number: 0x3841544631473634485A2D324733453220202020",
+    "Serial Number: 0x25250417",
+	"",
+	})
+
+	assert.Len(t, mem, 2)
+}
